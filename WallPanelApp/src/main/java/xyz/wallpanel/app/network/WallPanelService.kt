4e28f1ade62bc5413e852a50b704650997185e57
Jacek Hiszpanski <jacek.hiszpanski@tooploox.com>
2025-05-25 14:14:23 +0200

    increase mqtt delay before reconnect

--- WallPanelApp/src/main/java/xyz/wallpanel/app/network/WallPanelService.kt
+++ WallPanelApp/src/main/java/xyz/wallpanel/app/network/WallPanelService.kt
@@ -365,7 +365,7 @@ class WallPanelService : LifecycleService(), MQTTModule.MQTTListener {
             }
             if (!mqttConnecting) {
                 reconnectHandler.removeCallbacksAndMessages(null)
-                reconnectHandler.postDelayed(restartMqttRunnable, 60000)
+                reconnectHandler.postDelayed(restartMqttRunnable, 90000)
                 mqttConnecting = true
             }
         }
